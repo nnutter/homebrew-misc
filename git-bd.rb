5699210b94a7fd81d70bf84e27845ee13f8c6e61
Nathan Nutter <iam@nnutter.com>
2013-12-25 01:31:33 -0600

    update for v0.1.4

--- git-bd.rb
+++ git-bd.rb
@@ -2,19 +2,15 @@ require 'formula'
 
 class GitBd < Formula
     homepage 'https://github.com/nnutter/git-bd'
-    url 'https://github.com/nnutter/git-bd/archive/v0.1.2.zip'
+    url 'https://github.com/nnutter/git-bd/archive/v0.1.4.zip'
     head 'https://github.com/nnutter/git-bd.git'
-    sha1 '223fd28f7ab09aad3f57940d725d96ce13d04864'
+    sha1 '9456b15d1b79349a42d898b75348363547abdb9f'
 
     def install
         system "make install prefix=#{prefix}"
     end
 
-    def caveats; <<-EOS.undent
-        Add the following to your ~/.bash_profile:
-            if [ -f $(brew --prefix)/etc/git-bd.bashrc ]; then
-                source $(brew --prefix)/etc/git-bd.bashrc
-            fi
-        EOS
+    def caveats
+        system "make caveats prefix=#{prefix}"
     end
 end
