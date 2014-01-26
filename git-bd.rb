6e09f281ebba1f277ae53aaf7237f6c807f90e66
Nathaniel Nutter <iam@nnutter.com>
2014-01-26 02:18:40 -0600

    update git-bd to v0.1.6

--- git-bd.rb
+++ git-bd.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GitBd < Formula
     homepage 'https://github.com/nnutter/git-bd'
-    url 'https://github.com/nnutter/git-bd/archive/v0.1.4.zip'
+    url 'https://github.com/nnutter/git-bd/archive/v0.1.6.zip'
     head 'https://github.com/nnutter/git-bd.git'
-    sha1 '9456b15d1b79349a42d898b75348363547abdb9f'
+    sha1 'c53fada65d163cc0eab839995baea9ecdf7179a8'
 
     def install
         system "make install prefix=#{prefix}"
