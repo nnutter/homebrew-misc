37a12613446c0effc949dfdd13a2c0bf22fcf3fe
Nathaniel Nutter <iam@nnutter.com>
2014-01-27 22:51:52 -0600

    update git-pinch to v0.02

--- git-pinch.rb
+++ git-pinch.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class GitPinch < Formula
     homepage 'https://github.com/nnutter/git-pinch'
-    url 'https://github.com/nnutter/git-pinch/archive/v0.01.zip'
+    url 'https://github.com/nnutter/git-pinch/archive/v0.02.zip'
     head 'https://github.com/nnutter/git-pinch.git'
-    sha1 '2abcf8d30f4e3ba8fb1e590978d5f8644ed7d829'
+    sha1 'dad7106ed02c3223296e50574899cd5fb065c401'
 
     def install
         system "make install prefix=#{prefix}"
+        man.mkpath
+        man1.install ['git-pinch.1']
     end
 end
